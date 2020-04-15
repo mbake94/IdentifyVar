69bdddf86103264ec4c1ae159938a3dc4714eaf3
Miles Turner Baker <mbake94@gmail.com>
2020-04-15 00:46:24 -0700

    Add files via upload

---
+++ identifyvar/src/IdentifyVar.java
@@ -0,0 +1,20 @@
+public class IdentifyVar {
+    public static void main(String[] args) {
+        // write your code here
+        String country = "Kenya";
+        String capitalCity = "Nairobi";
+        double populationSize = 49.7;
+        double moneyConverter = 103.84;
+
+
+        String s = "Welcome to My first Java Program!";
+        System.out.println("Hey Mom and Dad!");
+        System.out.println("Here are some interesting details about the first country I'm visiting.");
+        System.out.println("Name: " + country);
+        System.out.println("Population Size: " +populationSize+ " million");
+        System.out.println("Capital: "+capitalCity);
+        System.out.println("Currency: One U.S dollar equals $"+moneyConverter+" Kenyan Shillings!!");
+        System.out.println("I'm so excited to visit the Nairobi Nation Park and feel the giraffes!! Talk later.");
+    }
+}
+
